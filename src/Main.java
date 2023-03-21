e0ebdff956f2222c3080ece96c06d8a06b3ec2d3
Kvini4ka <kvini4ka@mail.ru>
2023-03-21 23:34:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int coast = 13676;
+        int bonus = coast / 20;
+        System.out.printf("Количество миль =" + bonus);
+    }
+}
